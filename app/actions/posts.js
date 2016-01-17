11abdb7158381743155880b2537677f4d22eae92
Luke <lukemorton.dev@gmail.com>
2016-01-17 11:16:54 +0000

    Add post example to actions

--- app/actions/posts.js
+++ app/actions/posts.js
@@ -1,6 +1,12 @@
 import { actions } from 'tarnish';
-const { json, convertKeysToCamelCase } = actions.middleware;
+const { json, convertKeysToSnakeCase, convertKeysToCamelCase } = actions.middleware;
 
 export function loadPostSummaries() {
-  return actions.fetch('https://api/post_summaries.json', [json, convertKeysToCamelCase]);
+  return actions.get('https://api/post_summaries.json', [json, convertKeysToCamelCase]);
+}
+
+export function createPost(post) {
+  const url = 'https://api/post_summaries.json';
+  return actions.post(url, post, [convertKeysToSnakeCase, json],
+                                 [json, convertKeysToCamelCase]);
 }
