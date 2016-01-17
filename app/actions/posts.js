d9f65f6384584dc4f52be60b24ed6a089081874d
Luke <lukemorton.dev@gmail.com>
2016-01-17 13:43:11 +0000

    Move http action handling to specific namespace

--- app/actions/posts.js
+++ app/actions/posts.js
@@ -2,11 +2,11 @@ import { actions, actionMiddleware } from 'tarnish';
 const { json, convertKeysToSnakeCase, convertKeysToCamelCase } = actionMiddleware;
 
 export function loadPostSummaries() {
-  return actions.get('https://api/post_summaries.json', [json, convertKeysToCamelCase]);
+  return actions.http.get('https://api/post_summaries.json', [json, convertKeysToCamelCase]);
 }
 
 export function createPost(post) {
   const url = 'https://api/post_summaries.json';
-  return actions.post(url, post, [convertKeysToSnakeCase, json],
-                                 [json, convertKeysToCamelCase]);
+  return actions.http.post(url, post, [convertKeysToSnakeCase, json],
+                                      [json, convertKeysToCamelCase]);
 }
--- app/actions/sessions.js
+++ app/actions/sessions.js
@@ -2,5 +2,5 @@ import { actions, actionMiddleware } from 'tarnish';
 const { json } = actionMiddleware;
 
 export function createSession({ values }) {
-  return actions.post('https://api/sessions.json', values, [json], [json]);
+  return actions.http.post('https://api/sessions.json', values, [json], [json]);
 }
