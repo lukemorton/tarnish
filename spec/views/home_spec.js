b831e1e6743339a58543afbdcb9f7f2469f36585
Luke <lukemorton.dev@gmail.com>
2016-01-17 11:28:20 +0000

    Tidy up spec

--- spec/views/home_spec.js
+++ spec/views/home_spec.js
@@ -1,14 +1,16 @@
-feature('Home page',
-  scenario('Viewing post summaries',
-           when_i_visit_the_homepage
-           then_i_should_see_post_summaries));
+feature('Home page' function () {
+  scenario('Viewing post summaries', function () {
+    when_i_visit_the_homepage();
+    then_i_should_see_post_summaries();
+  });
 
-let homepage;
+  let homepage;
 
-function when_i_visit_the_homepage() {
-  homepage = page('home#Index', { postSummaries: factory.createList('postSummary', 3) });
-}
+  function when_i_visit_the_homepage() {
+    homepage = page('home#Index', { postSummaries: factory.createList('postSummary', 3) });
+  }
 
-function then_i_should_see_post_summaries() {
-  expect(homepage.find('post')).to.have.length(3);
-}
+  function then_i_should_see_post_summaries() {
+    expect(homepage.find('post')).to.have.length(3);
+  }
+});
