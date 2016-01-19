d974df852aafd644986e5c63fd7a76328858f3ff
Luke <lukemorton.dev@gmail.com>
2016-01-19 09:21:02 +0000

    Rename form validation to form validator

--- app/actions/todo.js
+++ app/actions/todo.js
@@ -1,7 +1,7 @@
 import { actions } from 'tarnish';
 
 export function addTask(form) {
-  return actions.form.validation(form, function ({ validate }) {
+  return actions.form.validator(form, function ({ validate }) {
     validate('task', { present: true });
   });
 }
