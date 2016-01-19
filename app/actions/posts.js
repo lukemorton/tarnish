483fdcae7e5d1d6c86f26023f9acd6c84c9d707f
Luke <lukemorton.dev@gmail.com>
2016-01-19 22:11:03 +0000

    Need to merge state

--- app/actions/posts.js
+++ app/actions/posts.js
@@ -1,9 +1,14 @@
 import { actions } from 'tarnish';
 
 export function loadPostSummaries() {
-  return actions.http.get('https://api/post_summaries.json');
+  return actions.http.get('https://api/post_summaries.json')
+                     .then(postSummaries => actions.state.merge({ postSummaries }));
 }
 
 export function createPost(post) {
-  return actions.http.post('https://api/post_summaries.json');
+  return actions.http.post('https://api/post_summaries.json').then(function (postSummary) {
+    return actions.state.merge(function ({ postSummaries }) {
+      return [...postSummaries, postSummary];
+    });
+  });
 }
