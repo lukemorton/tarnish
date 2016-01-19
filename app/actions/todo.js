c4d8ee5995f3f9baf9dec57f719e631d1b9696e6
Luke <lukemorton.dev@gmail.com>
2016-01-19 22:02:55 +0000

    Add example of adding todos if todo was valid

--- app/actions/todo.js
+++ app/actions/todo.js
@@ -1,7 +1,15 @@
 import { actions } from 'tarnish';
 
-export function addTask(form) {
+function taskValid(form) {
   return actions.form.validator(form, function ({ validate }) {
     validate('task', { present: true });
+  });
+}
+
+export function addTask(form) {
+  return taskValid(form).then(function ({ values }) {
+    return action.state.merge(function ({ todos }) {
+      return { todos: [...todos, values] };
+    });
   });
 }
