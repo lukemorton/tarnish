6d3d179296766e5523f1403a0702a426292ee29f
Luke <lukemorton.dev@gmail.com>
2016-01-20 10:01:47 +0000

    Remove more boilerplate!

--- app/actions/sessions.js
+++ app/actions/sessions.js
@@ -1,15 +1,7 @@
 import { actions } from 'tarnish';
 
-function handleResponse(form) {
-  return function ({ token, errors }) {
-    if (errors) {
-      return actions.form.validationErrors(form, errors);
-    } else {
-      return actions.state.merge({ page: 'home#Index', token });
-    }
-  };
-}
-
 export function createSession(form) {
-  return actions.http.post('https://api/sessions.json', form.values).then(handleResponse(form));
+  return actions.http.post('https://api/sessions.json', form).then(function ({ token }) {
+    return actions.state.merge({ page: 'home#Index', token })
+  });
 }
